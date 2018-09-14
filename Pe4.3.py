aed1399c2aeebf56be4f86fdbed09d4b1c677d18
Simon Wuite <simon.wuite@student.hu.nl>
2018-09-14 17:20:21 +0200

    test voor commit 2

--- Pe4.3.py
+++ Pe4.3.py
@@ -3,3 +3,4 @@ aantaluurgewerkt = float(input("Geef aan hoeveel uur je hebt gewerkt "))
 salaris = uurloon * aantaluurgewerkt
 print (f"{aantaluurgewerkt} uur werken levert {salaris} Euro op!")
 # test voor git
+# weer nieuwe git commit test
